df3f0beb0ae08f6ba358d976f01929847b40b56d
Sasha Dyachkov <sanya7765045@gmail.com>
2023-05-30 21:52:23 +0300

    Domashka

---
+++ OneDrive/Рабочий стол/Lekciyi DZ/4_semenar_c#/DZ/29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Эта функция генерирует массив из 8 случайных чисел в диапазоне от 0 до 100 включительно
+int[] ArrayRand(){
+    int[] digit = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        digit[i] = new Random().Next(0, 101);
+        //Console.Write($"{coins[i]} ");    
+    }
+    return digit;
+}
+
+// Эта функция выводит на консоль элементы массива
+void printArr(int []arr){
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"{arr[i]} ");    
+    }
+}
+
+// Очистка консоли перед выполнением программы
+Console.Clear();
+
+// Генерация и вывод массива на консоль
+int[] arrNum = ArrayRand();
+Console.WriteLine();
+printArr(arrNum);
+Console.WriteLine();
